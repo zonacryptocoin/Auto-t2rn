402b119247e48d5092534478ac68cb559a646ef5
zonacryptocoin <98642676+zonacryptocoin@users.noreply.github.com>
2025-03-20 11:15:35 +0700

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,11 @@
+# keys.py
+
+# Daftar private key (Setiap private key harus berupa string)
+private_keys = [
+    ""
+]
+
+# Daftar label (Setiap label sesuai dengan private key yang diberikan)
+labels = [
+    "Wallet1"
+]
