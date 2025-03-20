838164d357dba016c6e65de949699d92116a3bac
zonacryptocoin <98642676+zonacryptocoin@users.noreply.github.com>
2025-03-20 11:13:29 +0700

    Create network_config.py

---
+++ network_config.py
@@ -0,0 +1,14 @@
+# network_config.py
+
+networks = {
+    'Base': {
+        'rpc_url': 'https://sepolia.base.org', 
+        'chain_id': 84532, 
+        'contract_address': '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
+    },
+    'OP Sepolia': {
+        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
+        'chain_id': 11155420,
+        'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
+    },
+}
